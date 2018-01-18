a54e8592ba48f97ddc75106faec1ca7564115e8a
Hakaba Hitoyo <tsukadayoshio@gmail.com>
2018-01-18 10:02:55 +0900

    Show domain in Pleroma instances

--- client/pleroma-instances.js
+++ client/pleroma-instances.js
@@ -48,6 +48,7 @@ for (cn = 0; cn < instances.length; cn ++) {
 		'instance-preview.html?' + instance.domain + '" target="distsn-instance-preview">' +
 		(instance.title? escapeHtml (instance.title): instance.domain) +
 		'</a>' +
+		(instance.title && instance.title !== instance.domain? '<br>' + instance.domain: '') +
 		'</p>';
 	html += instance_html;
 }
