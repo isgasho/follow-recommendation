5910a23f41170e7f1d047a9ea9da7818996af31d
Hakaba Hitoyo <example@example.com>
2018-09-23 12:35:13 +0900

    refactor

--- server/pleroma-instances-cron.cpp
+++ server/pleroma-instances-cron.cpp
@@ -6,6 +6,9 @@
 #include <vector>
 #include <sstream>
 #include <set>
+
+#include <socialnet-1.h>
+
 #include "picojson.h"
 #include "distsn.h"
 
@@ -118,17 +121,17 @@ static void get_host_nodeinfo (
 	bool &a_gopher,
 	bool &a_suggestions,
 	bool &a_media_proxy,
-	Http &http
+	socialnet::Http &http
 ) {
 	string reply = http.perform (string {"https://"} + host + string {"/nodeinfo/2.0.json"});
 
 	picojson::value json_value;
 	string error = picojson::parse (json_value, reply);
 	if (! error.empty ()) {
-		throw (HostException {__LINE__});
+		throw (socialnet::HostException {__LINE__});
 	}
 	if (! json_value.is <picojson::object> ()) {
-		throw (HostException {__LINE__});
+		throw (socialnet::HostException {__LINE__});
 	}
 	auto json_object = json_value.get <picojson::object> ();
 	if (json_object.find (string {"openRegistrations"}) != json_object.end ()) {
@@ -180,25 +183,25 @@ static void get_host_nodeinfo (
 }
 
 
-static void get_host_statusnet_config (string host, bool &a_scope_options, unsigned int &a_text_limit, Http &http)
+static void get_host_statusnet_config (string host, bool &a_scope_options, unsigned int &a_text_limit, socialnet::Http &http)
 {
 	string reply = http.perform (string {"https://"} + host + string {"/api/statusnet/config"});
 
 	picojson::value json_value;
 	string error = picojson::parse (json_value, reply);
 	if (! error.empty ()) {
-		throw (HostException {__LINE__});
+		throw (socialnet::HostException {__LINE__});
 	}
 	if (! json_value.is <picojson::object> ()) {
-		throw (HostException {__LINE__});
+		throw (socialnet::HostException {__LINE__});
 	}
 	auto json_object = json_value.get <picojson::object> ();
 	if (json_object.find (string {"site"}) == json_object.end ()) {
-		throw (HostException {__LINE__});
+		throw (socialnet::HostException {__LINE__});
 	}
 	auto site_value = json_object.at (string {"site"});
 	if (! site_value.is <picojson::object> ()) {
-		throw (HostException {__LINE__});
+		throw (socialnet::HostException {__LINE__});
 	}
 	auto site_object = site_value.get <picojson::object> ();
 
@@ -229,36 +232,17 @@ static void get_host_statusnet_config (string host, bool &a_scope_options, unsig
 }
 
 
-static Host for_host (string domain)
+static Host for_host (shared_ptr <socialnet::Host> socialnet_host)
 {
-	Http http;
-	string reply_string;
-
-	try {
-		reply_string = http.perform (string {"https://"} + domain + string {"/api/pleroma/emoji"});
-	} catch (HttpException e) {
-		cerr << domain << " has no /api/pleroma/emoji" << endl;
-		throw (HostException {__LINE__});
-	}
-
-	picojson::value reply_value;
-	string error = picojson::parse (reply_value, reply_string);
-	if (! error.empty ()) {
-		cerr << error << endl;
-		throw (HostException {__LINE__});
-	}
+	socialnet::Http &http {* socialnet_host->http};
+	string domain = socialnet_host->host_name;
 
 	string title;
-	try {
-		title = get_host_title (domain, http);
-	} catch (HostException e) {
-		/* Do nothing. */
-	}
-
+	string description;
 	string thumbnail;
 	try {
-		thumbnail = get_host_thumbnail (domain, http);
-	} catch (HostException e) {
+		socialnet_host->get_profile (title, description, thumbnail);
+	} catch (socialnet::HostException e) {
 		/* Do nothing. */
 	}
 
@@ -276,7 +260,7 @@ static Host for_host (string domain)
 		host.gopher = gopher;
 		host.who_to_follow = suggestions;
 		host.media_proxy = media_proxy;
-	} catch (ExceptionWithLineNumber e) {
+	} catch (socialnet::ExceptionWithLineNumber e) {
 		cerr << e.line << endl;
 	}
 
@@ -286,7 +270,7 @@ static Host for_host (string domain)
 		get_host_statusnet_config (domain, scope_options, text_limit, http);
 		host.scope_options = scope_options;
 		host.text_limit = text_limit;
-	} catch (ExceptionWithLineNumber e) {
+	} catch (socialnet::ExceptionWithLineNumber e) {
 		cerr << e.line << endl;
 	}
 	
@@ -296,21 +280,17 @@ static Host for_host (string domain)
 
 int main (int argc, char **argv)
 {
-	set <string> domains;
-	domains = get_international_hosts ();
+	auto socialnet_hosts = socialnet::get_hosts ();
 
 	vector <Host> hosts;
-	for (auto domain: domains) {
-		cerr << domain << endl;
+	for (auto socialnet_host: socialnet_hosts) {
+		cerr << socialnet_host->host_name << endl;
 		try {
-			Host host = for_host (string {domain});
+			Host host = for_host (socialnet_host);
 			hosts.push_back (host);
-		} catch (HttpException e) {
+		} catch (socialnet::HostException e) {
 			/* Nothing. */
-			cerr << "HttpException " << e.line << endl;
-		} catch (HostException e) {
-			/* Nothing. */
-			cerr << "HostException " << e.line << endl;
+			cerr << "socialnet::HostException " << e.line << endl;
 		}
 	}
 	
