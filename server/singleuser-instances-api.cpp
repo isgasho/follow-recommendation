bde78d0853a1c815e1ee8b3fc787d40a9bd75072
Hakaba Hitoyo <hakabahitoyo@example.com>
2018-12-19 15:59:18 +0900

    #65

---
+++ server/singleuser-instances-api.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main ()
+{
+	cout << "Access-Control-Allow-Origin: *" << endl;
+	cout << "Content-type: application/json" << endl << endl;
+	cout << "[]" << endl;
+}
+
